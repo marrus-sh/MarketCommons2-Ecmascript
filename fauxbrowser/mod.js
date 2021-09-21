84dc4aeea26b5bd7d5885957b840df9a414535e3
kibigo! <marrus-sh@users.noreply.github.com>
2021-09-21 00:40:47 -0700

    Lint and fmt fauxbrowser
    This required duplicating the TreeWalker polyfill as a file in this
      repository, as it is a historical piece of JavaScript code which
      definitely does not typecheck or lint properly.

--- fauxbrowser/mod.js
+++ fauxbrowser/mod.js
@@ -3,29 +3,28 @@
 //
 //  Copyright © 2021 Margaret KIBI.
 //
-//  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
-//  If a copy of the MPL was not distributed with this file, You can obtain one at <https://mozilla.org/MPL/2.0/>.
+//  This Source Code Form is subject to the terms of the Mozilla
+//    Public License, v. 2.0.
+//  If a copy of the MPL was not distributed with this file, You can
+//    obtain one at <https://mozilla.org/MPL/2.0/>.
+//
+//  ___________________________________________________________________
+//
+//  This module spoofs a browser well enough that Market Commons ⅠⅠ –
+//    Ecmascript will work in Deno.
+//
+//  You’ll need to import this module, or provide your own solution, to
+//    get things working on the command line.
 
-/**
- *  Spoofs a browser well enough that Market Commons ⅠⅠ – Ecmascript
- *    will work in deno.
- *
- *  You’ll need to import this module, or provide your own solution, to
- *    get things working on the command line.
- *
- *  @module MarketCommons2/fauxbrowser
- */
-
-import "./window.js"
-import
-	//  This is an old, archived script but it seems to do the job.
-	"https://raw.githubusercontent.com/Krinkle/dom-TreeWalker-polyfill/v0.2.0/src/TreeWalker-polyfill.js"
+import "./window.js";
+import //  This is an old, archived script but it seems to do the job.
+"./treewalker.js";
 
 //  The above `TreeWalker` polyfill isn’t very good and only assigns
 //    `createTreeWalker()` as a method on `window.document`.
 //  This moves it to the prototype, so that it will be available in
 //    documents created through `XMLParser`.
-const DocumentPrototype = Object.getPrototypeOf(window.document)
-if ( DocumentPrototype.createTreeWalker == null ) {
-	DocumentPrototype.createTreeWalker = document.createTreeWalker
+const DocumentPrototype = Object.getPrototypeOf(window.document);
+if (DocumentPrototype.createTreeWalker == null) {
+	DocumentPrototype.createTreeWalker = document.createTreeWalker;
 }
--- fauxbrowser/window.js
+++ fauxbrowser/window.js
@@ -3,30 +3,36 @@
 //
 //  Copyright © 2021 Margaret KIBI.
 //
-//  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
-//  If a copy of the MPL was not distributed with this file, You can obtain one at <https://mozilla.org/MPL/2.0/>.
+//  This Source Code Form is subject to the terms of the Mozilla
+//    Public License, v. 2.0.
+//  If a copy of the MPL was not distributed with this file, You can
+//    obtain one at <https://mozilla.org/MPL/2.0/>.
+//
+//  ___________________________________________________________________
+//
+//  This module sets up a `window` and `document` for scripts which
+//    expect it to be present, and assigns `DOMParser` and
+//    `XMLSerializer` on the former.
+//
+//  Import this module *first* (before any modules wot expect these
+//    things to be present).
 
-/**
- *  Sets up a `window` and `document` for scripts which expect it to
- *    be present, and assigns `DOMParser` and `XMLSerializer` on the
- *    former.
- *
- *  Import this module *first* (before any modules wot expect these
- *    things to be present).
- *
- *  @module MarketCommons2/fauxbrowser/window
- */
+//deno-lint-ignore-file ban-ts-comment
 
-import { x·h·t·m·lNamespace } from "../names.js"
-import { DOMParser, XMLSerializer }
-	from "https://esm.sh/@xmldom/xmldom@0.7.3"
+import { x·h·t·m·lNamespace } from "../names.js";
+import {
+	DOMParser,
+	XMLSerializer,
+} from "https://esm.sh/@xmldom/xmldom@0.7.3";
 
-globalThis.window ??= globalThis
-window.DOMParser ??= DOMParser
-window.XMLSerializer ??= XMLSerializer
+//@ts-ignore
+globalThis.window ??= globalThis;
+window.DOMParser ??= DOMParser;
+window.XMLSerializer ??= XMLSerializer;
+//@ts-ignore
 window.document ??=
 	//  A blank X·H·T·M·L page.
-	(new DOMParser).parseFromString(
-		`<html xmlns="${ x·h·t·m·lNamespace }"><head/><body/></html>`,
-		"application/xhtml+xml"
-	)
+	(new DOMParser()).parseFromString(
+		`<html xmlns="${x·h·t·m·lNamespace}"><head/><body/></html>`,
+		"application/xhtml+xml",
+	);
